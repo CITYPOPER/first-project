04511cdcc7cb64594b9a76474e153ca084550039
CITYPOPER <harc3266@gmail.com>
2022-04-13 22:43:31 +0900

    # WARNING: head commit changed in the meantime
    commit test

---
+++ baekjoon/src/baekjoon/baekjoonBronze5.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class baekjoonBronze5 {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		int sum = a+b;
+		
+		System.out.println(sum);
+		
+
+	}
+
+}
